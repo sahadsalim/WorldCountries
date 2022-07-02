0e18531ff3f520bcb7153866fc21395927af5a1a
sahadsha <sahads9745@gmail.com>
2022-07-02 18:55:14 +0530

    "styles updated"

--- world_countries/src/component/LIst/continent-list.js
+++ world_countries/src/component/LIst/continent-list.js
@@ -6,7 +6,7 @@ const ContinentList = (props) => {
       <div
         key={index}
         onClick={() => props.onContinentSelect(c)}
-        className={`block px-4 py-2 mt-2 text-sm font-bold  bg-gray-200 rounded-lg dark-mode:bg-gray-700 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline ${props.selected===c?"border-2 border-blue-700 text-blue-600":"text-gray-900"}`}
+        className={`block px-4 py-2 mt-2 text-sm font-bold  bg-gray-200 rounded-lg dark-mode:bg-gray-700 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline ${props.selected===c?"border-2 border-red-600 text-red-600 text-center":"text-gray-900"}`}
         style={{ cursor: "pointer" }}
       >
         {c}
