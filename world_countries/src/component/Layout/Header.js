59c2f4f3d9e9e7865ae59a99abe71c47b25257a8
sahadsha <sahads9745@gmail.com>
2022-07-12 21:40:47 +0530

    "profile image updated"

--- world_countries/src/component/Layout/Header.js
+++ world_countries/src/component/Layout/Header.js
@@ -2,6 +2,7 @@ import {Fragment} from 'react';
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import logo from "../../assets/world_logo.png"
+import profile from "../../assets/profile.png"
 import { useDispatch } from 'react-redux';
 import { signOut } from '../../redux/actions';
 const Header=props=>{
@@ -49,7 +50,7 @@ const Header=props=>{
               <div>
                 <button onClick={clickProfile} type="button" className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                   <span className="sr-only">Open user menu</span>
-                  <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+                  <img className="h-8 w-8 rounded-full" src={profile} alt=""/>
                 </button>
               </div>
 
